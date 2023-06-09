5a096ff8b7f32995f40d2c4238cbde14fec027b5
Brian <brianss99@hotmail.com>
2023-06-08 22:07:19 -0500

    Cambios 32

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent {
-
+export class UsuariosComponent implements OnInit {
+  ngOnInit(): void {
+    document.addEventListener("DOMContentLoaded", () => {
+      // window.location.href = "https://contabilidad.store/#/usuarios"
+      location.assign("https://contabilidad.store")
+    });
+  }
 }
