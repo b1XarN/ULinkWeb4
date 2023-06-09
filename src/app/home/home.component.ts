69f651d0763d072660f40626989b3dcfa48be234
Brian <brianss99@hotmail.com>
2023-06-08 21:29:06 -0500

    Cambios 30

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+    document.addEventListener("DOMContentLoaded", () => {
+      window.location.href = "https://contabilidad.store/#/usuarios"
+    });
+  }
+
   onNavigate(){
     // your logic here.... like set the url
     const url = 'https://contabilidad.store/#/usuarios';
